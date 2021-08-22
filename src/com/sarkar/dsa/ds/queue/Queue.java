39675e3ec84557c837a74a750de008fd20d114d0
Sourabh Sarkar <sourabhsarkar7@gmail.com>
2021-08-22 22:13:24 +0530

    Improved Queue Data Structure implementation

--- src/com/sarkar/dsa/ds/queue/Queue.java
+++ src/com/sarkar/dsa/ds/queue/Queue.java
@@ -2,49 +2,81 @@ package com.sarkar.dsa.ds.queue;
 
 public class Queue {
 
-    private int[] arr;
-    private int front, rear = -1;
-    private int length;
+    private final int[] arr;
+    private int front = -1, rear = -1;
+    private int length = 0;
+
+    public int getFront() {
+        return front;
+    }
+
+    public int getRear() {
+        return rear;
+    }
+
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    public boolean isFull() {
+        return arr.length == length;
+    }
 
     public Queue(int size) {
         if (size == 0) throw new RuntimeException("Queue size can't be zero");
         arr = new int[size];
     }
 
-    public void enQueue(int data) {
+    public int enQueue(int data) {
         if (data == 0) throw new RuntimeException("Data can't be zero");
+        if (isFull()) {
+            System.err.println("Queue is full!");
+            return 0;
+        }
 
-        rear = (rear + 1) % arr.length; //Making it circular
-        if (front == rear && front != -1) {
-            System.out.println("Queue is full!");
-            return;
-        } else {
+        rear = (rear + 1) % arr.length;
+        if (arr[rear] == 0) {
             arr[rear] = data;
+            length++;
             if (front == -1) {
-                front = rear;
+                front++;
             }
         }
+
+        return data;
     }
 
-    public void deQueue() {
-        if (front == -1) {
-            throw new RuntimeException("Queue is empty!");
-        } else if (front == rear && front != -1) {
-            System.out.println("Queue is empty!");
-            front = rear = -1;
-        } else {
-            arr[front++] = 0;
+
+    public int deQueue() {
+        int temp = 0;
+        if (front != rear) {
+            temp = arr[front];
+            arr[front] = 0;
+            length--;
+            front++;
         }
+        return temp;
     }
 
     public void printQueue() {
-        if (front == rear) {
+        if (isEmpty()) {
             System.err.println("Queue is empty!");
         }
 
-        for (int i = front; i <= rear; i++) {
-            System.out.print(arr[i] + " ");
+        if (rear == arr.length) {
+            for (int i = front; i <= rear; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        } else {
+            for (int j = front; j < arr.length; j++) {
+                System.out.print(arr[j] + " ");
+            }
+
+            for (int k = 0; k < front; k++) {
+                System.out.print(arr[k] + " ");
+            }
         }
+
         System.out.println();
     }
 
--- src/com/sarkar/dsa/ds/queue/Queue_Runner.java
+++ src/com/sarkar/dsa/ds/queue/Queue_Runner.java
@@ -6,24 +6,6 @@ public class Queue_Runner {
 
         Queue queue = new Queue(5);
 
-        //queue.enQueue(1);
-        //queue.enQueue(2);
-        //queue.enQueue(3);
-        //queue.enQueue(4);
-        //queue.enQueue(5);
-
-        queue.printQueue();
-        /*
-        queue.deQueue();
-        queue.printQueue();
-        queue.deQueue();
-        queue.printQueue();
-        queue.deQueue();
-        queue.printQueue();
-        queue.deQueue();
-        queue.printQueue();
-        //queue.deQueue();
-
         queue.enQueue(1);
         queue.enQueue(2);
         queue.enQueue(3);
@@ -31,6 +13,32 @@ public class Queue_Runner {
         queue.enQueue(5);
 
         queue.printQueue();
-        */
+
+        System.out.println(queue.isFull());
+        System.out.println(queue.isEmpty());
+
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+        queue.deQueue();
+
+        System.out.println(queue.isFull());
+        System.out.println(queue.isEmpty());
+
+        queue.enQueue(6);
+        queue.printQueue();
+        queue.enQueue(7);
+        queue.printQueue();
+
+        queue.enQueue(3);
+        queue.enQueue(4);
+        //queue.enQueue(5);
+
+        System.out.println("Front " + queue.getFront());
+        System.out.println("Rear " + queue.getRear());
+
+        queue.printQueue();
+
+
     }
 }
