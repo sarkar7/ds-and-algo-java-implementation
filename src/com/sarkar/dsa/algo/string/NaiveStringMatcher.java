aa28e6b2e186913c28d7e5ef1597619d21ecb979
Sourabh Sarkar <sourabhsarkar7@gmail.com>
2021-09-11 16:47:42 +0530

    Naive String Matching Algorithm Implementation

---
+++ src/com/sarkar/dsa/algo/string/NaiveStringMatcher.java
@@ -0,0 +1,30 @@
+package com.sarkar.dsa.algo.string;
+
+/**
+ *
+ * Time Complexity - Big O(n - m + 1)m
+ *
+ */
+
+public class NaiveStringMatcher {
+
+    public static int findWord(String text, String str) {
+        int numberOfOccurrence = 0;
+        int str_length = str.length();
+        int text_length = text.length();
+        for (int i = 0; i < text_length - str_length; i++) {
+            if (match (text.substring(i, i + str_length), str)) {
+                numberOfOccurrence++;
+            }
+        }
+        return numberOfOccurrence;
+    }
+
+    private static boolean match(String text, String str) {
+        for (int j = 0; j < str.length(); j++) {
+            if (text.charAt(j) != str.charAt(j)) return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/com/sarkar/dsa/algo/string/Runner.java
@@ -0,0 +1,12 @@
+package com.sarkar.dsa.algo.string;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        String text = "bbabccccabbbbcbabcaaa";
+        String str = "abc";
+
+        System.out.println(NaiveStringMatcher.findWord(text, str));
+    }
+
+}
