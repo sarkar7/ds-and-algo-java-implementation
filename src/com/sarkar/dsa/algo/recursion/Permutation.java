dfcbca011bc4b6d3db110a0d825ff0c9ab64f984
Sourabh Sarkar <sourabhsarkar7@gmail.com>
2021-11-08 14:09:14 +0530

    Added String Permutation Implementation

---
+++ src/com/sarkar/dsa/algo/recursion/Permutation.java
@@ -0,0 +1,69 @@
+package com.sarkar.dsa.algo.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutation {
+
+	public static void main(String[] args) {
+
+		// doPermutationRecursively("", "abc");
+		// List<String> list = permutationList("", "abc");
+		// System.out.println(list);
+
+		System.out.println(permutationCount("", "abc"));
+
+	}
+
+	static void doPermutationRecursively(String p, String up) {
+		if (up.isEmpty()) {
+			System.out.println(p);
+			return;
+		}
+
+		char ch = up.charAt(0);
+
+		for (int i = 0; i <= p.length(); i++) {
+			String f = p.substring(0, i);
+			String s = p.substring(i, p.length());
+			doPermutationRecursively(f + ch + s, up.substring(1));
+		}
+	}
+
+	static List<String> permutationList(String p, String up) {
+
+		if (up.isEmpty()) {
+			List<String> list = new ArrayList<>();
+			list.add(p);
+			return list;
+		}
+
+		char ch = up.charAt(0);
+		List<String> ans = new ArrayList<>();
+
+		for (int i = 0; i <= p.length(); i++) {
+			String f = p.substring(0, i);
+			String s = p.substring(i, p.length());
+			ans.addAll(permutationList(f + ch + s, up.substring(1)));
+		}
+
+		return ans;
+	}
+
+	static int permutationCount(String p, String up) {
+		if (up.isEmpty()) {
+			System.out.println(p);
+			return 1;
+		}
+
+		char ch = up.charAt(0);
+		int count = 0;
+		for (int i = 0; i <= p.length(); i++) {
+			String f = p.substring(0, i);
+			String s = p.substring(i, p.length());
+			count += permutationCount(f + ch + s, up.substring(1));
+		}
+
+		return count;
+	}
+}
--- src/com/sarkar/dsa/algo/recursion/PowerOfTwo.java
+++ src/com/sarkar/dsa/algo/recursion/PowerOfTwo.java
@@ -2,23 +2,19 @@ package com.sarkar.dsa.algo.recursion;
 
 public class PowerOfTwo {
 
+	public static void main(String[] args) {
+		System.out.println(getPowerOfTwo(5));
+	}
 
-    public static void main(String[] args) {
-        System.out.println(
+	private static int getPowerOfTwo(int i) {
 
-                getPowerOfTwo(5)
-        );
-    }
+		if (i == 0) {
+			return 1;
+		} else if (i == 1) {
+			return 2;
+		}
 
-    private static int getPowerOfTwo(int i) {
+		return 2 * getPowerOfTwo(i - 1);
 
-        if(i == 0) {
-            return 1;
-        } else if (i == 1) {
-            return 2;
-        }
-
-        return 2 * getPowerOfTwo(i - 1);
-
-    }
+	}
 }
